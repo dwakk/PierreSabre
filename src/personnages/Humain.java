d648b520f2adf448133c403c1a403eb9242e6474
dwakk <111376922+dwakk@users.noreply.github.com>
2025-11-07 08:54:06 +0100

    Création de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,55 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonPref;
+	private int argent;
+
+	public Humain(String nom, String boissonPref, int argent) {
+		super();
+		this.nom = nom;
+		this.boissonPref = boissonPref;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getBoissonPref() {
+		return boissonPref;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+
+	private void parler(String texte) {
+		System.out.println("(" + nom + ") - " + texte);
+	}
+
+	public void direBonjour() {
+		parler("Bonjour! Je m'appelle " + nom + " et j'aime boire du " + boissonPref + ".");
+	}
+
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boissonPref + "! GLOUPS!");
+	}
+
+	private void gagnerArgent(int gain) {
+		this.argent += gain;
+	}
+
+	private void perdreArgent(int perte) {
+		this.argent -= perte;
+	}
+
+	public void acheter(String bien, int prix) {
+		if (prix <= argent) {
+			parler("J'ai " + argent + " en poche. Je vais pouvoir m'offrir un " + bien + " à " + prix + " sous");
+			perdreArgent(prix);
+		} else {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir un " + bien + " à " + prix + " sous");
+		}
+	}
+}

a6cd7c9d03be30a00d69725f982b6466cc128ca5
dwakk <111376922+dwakk@users.noreply.github.com>
2025-11-07 09:14:20 +0100

    Création de la classe Yakuza

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -2,6 +2,7 @@ package histoire;
 
 import personnages.Commercant;
 import personnages.Humain;
+import personnages.Yakuza;
 
 public class HistoireTP4 {
 	public static void main(String[] args) {
@@ -17,5 +18,9 @@ public class HistoireTP4 {
 		marco.seFaireExtorquer();
 		marco.recevoir(15);
 		marco.boire();
+		
+		Yakuza yaku = new Yakuza("Yaku Le Noir", "whiskey", 30, "Warsong");
+		yaku.direBonjour();
+		yaku.extorquer(marco);
 	}
 }
--- src/personnages/Commercant.java
+++ src/personnages/Commercant.java
@@ -15,7 +15,7 @@ public class Commercant extends Humain {
 	
 	public void recevoir(int argent) {
 		this.argent += argent;
-		parler(argent + " sous ! Je te remercie généreux donateur!");
+		parler(argent + " sous! Je te remercie généreux donateur!");
 	}
 
 }

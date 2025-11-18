ec935f3af0b1644ec60d880bd3adced118814159
dwakk <111376922+dwakk@users.noreply.github.com>
2025-11-18 09:39:48 +0100

    Ajout des samourai

---
+++ src/personnages/Samourai.java
@@ -0,0 +1,20 @@
+package personnages;
+
+public class Samourai extends Ronin{
+	String seigneur;
+	
+	public Samourai(String seigneur, String nom, String boissonPref, int argent) {
+		super(nom, boissonPref, argent);
+		this.seigneur = seigneur;
+	}
+	
+	@Override
+	public void direBonjour() {
+		super.direBonjour();
+		parler("Je suis fier de servir le seigneur " + seigneur + ".");
+	}
+	
+	public void boire(String boisson) {
+		parler("Qu'est-ce que je vais choisir comme boisson ? Tiens je vais prendre du " + boisson + ".");
+	}
+}
